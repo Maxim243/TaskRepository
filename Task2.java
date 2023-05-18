881f375ab9c82c502a2f5b3b5d3be5af10832577
Maxim Korsakov <vip.maxim.korsakov.2001@gmail.com>
2023-05-18 08:57:23 +0300

    Divided 1 method in 3 methods.

--- Task2.java
+++ Task2.java
@@ -3,22 +3,33 @@ package org.exampl;
 import java.util.Arrays;
 
 public class Task2 {
+    private static int maxIndex = 0;
+    private static int minIndex = 0;
 
-    public static int[] reverseItems(int[] array) {
-        if (array.length <= 1) return array;
-        int temp;
-        int minIndex = 0;
-        int maxIndex = 0;
+    public static int minIndex(int[] array) {
         for (int i = 1; i < array.length; i++) {
             if (array[i] < array[minIndex]) {
                 minIndex = i;
             }
         }
+        return minIndex;
+    }
+
+    public static int maxIndex(int[] array) {
         for (int i = 1; i < array.length; i++) {
             if (array[i] > array[maxIndex]) {
                 maxIndex = i;
             }
         }
+        return maxIndex;
+    }
+
+    public static int[] reverseItems(int[] array) {
+        if (array.length <= 1) return array;
+        int temp;
+        minIndex = minIndex(array);
+        maxIndex = maxIndex(array);
+
         if (maxIndex < minIndex) return array;
         int firstElement = minIndex;
         int lastElement = maxIndex;
@@ -33,9 +44,8 @@ public class Task2 {
         return array;
     }
 
-
     public static void main(String[] args) {
-        int[] arrayExample = {2, 3, 1, 4, 6, 5, 13, 3};
+        int[] arrayExample = {2, 3, 1, 2, 3, 4, 13, 3};
         System.out.println(Arrays.toString(Task2.reverseItems(arrayExample)));
     }
 }
